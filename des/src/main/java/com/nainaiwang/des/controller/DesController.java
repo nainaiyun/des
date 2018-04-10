a84c03d2dda40350ca092f530a66230ad4a977f8
yanhaopeng <1071477992@qq.com>
2018-04-10 09:34:47 +0800

    普通修改

--- des/src/main/java/com/nainaiwang/des/controller/DesController.java
+++ des/src/main/java/com/nainaiwang/des/controller/DesController.java
@@ -45,7 +45,7 @@ public class DesController {
         String type = request.getParameter("type");
         LOGGER.info("秘钥类型：" + type);
         /**获取我方公钥*/
-        Key pub_key = RsaUtil.getKey(PUBLIC_KEY_FILE);
+        Key pub_key = (Key) RsaUtil.getKey(PUBLIC_KEY_FILE,"pub");
         String mch_no = "4100000109";
         OutputStream outputStream = response.getOutputStream();
         /**写入成功应答码*/
@@ -69,28 +69,23 @@ public class DesController {
     @ResponseBody
     @RequestMapping(value = "/setkey")
     public String setKey(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        X509Certificate[] certs = (X509Certificate[]) request.getAttribute("javax.servlet.request.X509Certificate");
-        byte[] certTrans = new byte[]{};
-        if (certs != null && certs.length != 0) {
-            certTrans = certs[0].getEncoded();
-        }
-
         String url = "http://128.192.182.51:7001/merchant/KeyTransfer/jh8888";
         //String url = "http://localhost:7001/des/getkey";
         String mch_no = "4100000109";
         String type_no = request.getParameter("type");
-        String type = "type=" + type_no;
         HttpClient httpClient = new HttpClient();
         PostMethod method = new PostMethod(url);
-        method.addParameter("type", type);
+        method.addParameter("type", type_no);
         httpClient.getParams().setSoTimeout(3000);
         int statusCode = httpClient.executeMethod(method);
+        LOGGER.info("响应码："+statusCode);
         if (statusCode != HttpStatus.SC_OK) {
             //错误处理
             LOGGER.error("获取失败，请稍后重试");
             return "获取失败，请稍后重试";
         } else {
             byte[] data = method.getResponseBody();
+            LOGGER.info("data为"+Base64.encodeBase64String(data));
             byte[] tmp = new byte[6];
             System.arraycopy(data, 0, tmp, 0, tmp.length);
             String return_code = new String(tmp);
@@ -102,9 +97,9 @@ public class DesController {
                 String key = DesedeUtil.decrypt(tmp, mch_no); //使用约定密钥对传输的密钥进行解密
                 LOGGER.info("请求的解密后的公钥为：" + key);
                 if ("pub".equals(type_no)) {
-                    RsaUtil.setKey(key, JH_PUBLIC_KEY_FILE);
+                    RsaUtil.setKey(key, JH_PUBLIC_KEY_FILE,type_no);
                 } else if ("des".equals(type_no)) {
-                    RsaUtil.setKey(key, JH_DES_KEY_FILE);
+                    RsaUtil.setKey(key, JH_DES_KEY_FILE,type_no);
                 }
                 return "获取成功！";
             } else {
@@ -124,20 +119,22 @@ public class DesController {
             LOGGER.info("字符集不是GBK");
             requestMessage =new String(requestMessage.getBytes(), "GBK");
         }
+        LOGGER.info("请求报文为："+requestMessage);
         /**获取我方私钥*/
-        PrivateKey privateKey = (PrivateKey) RsaUtil.getKey(PRIVATE_KEY_FILE);
-        /**私钥加密请求报文*/
-        byte[] encodedText = RsaUtil.jhEncrypt(requestMessage, JH_PUBLIC_KEY_FILE);
+        PrivateKey privateKey = (PrivateKey) RsaUtil.getKey(PRIVATE_KEY_FILE,"pri");
         /**签名*/
         byte[] signature = RsaUtil.sign(privateKey, requestMessage.getBytes());
+
+        /**建行公钥加密请求报文*/
+//        byte[] encodedText = RsaUtil.jhEncrypt(requestMessage, JH_PUBLIC_KEY_FILE);
+
         /**获取对称秘钥*/
-        Key desKey = RsaUtil.getKey(JH_DES_KEY_FILE);
-        String dk = new String(Base64.encodeBase64(desKey.getEncoded()));
+        String desKey = (String) RsaUtil.getKey(JH_DES_KEY_FILE,"des");
         /**des加密*/
-        byte[] requestMessagePriKeyDes = DesedeUtil.encrypt(Base64.encodeBase64String(encodedText), dk);
+        byte[] requestMessagePriKeyDes = DesedeUtil.encrypt(requestMessage, desKey);
 
         /**建行接口地址*/
-        String url = "";
+        String url = "http://128.192.182.51: 7001/merchant/Tran/jh8888";
         HttpClient httpClient = new HttpClient();
         PostMethod method = new PostMethod(url);
         method.addParameter("xml", Base64.encodeBase64String(requestMessagePriKeyDes));
@@ -149,6 +146,9 @@ public class DesController {
             LOGGER.error("获取失败，请稍后重试");
             return "测试失败";
         } else {
+            byte[] data = method.getResponseBody();
+            LOGGER.info("data为:"+Base64.encodeBase64String(data));
+
             return "测试成功！";
         }
     }
@@ -156,7 +156,7 @@ public class DesController {
     @RequestMapping(value = "/receiveRequest", method = RequestMethod.POST)
     public String receiveRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         /**获取请求报文以及数字签名*/
-        String requestMessage = request.getParameter("requestMessage");
+        String requestMessage = request.getParameter("xml");
         String signature = request.getParameter("signature");
         if (!requestMessage.equals(new String(requestMessage.getBytes(), "GBK"))) {
             LOGGER.info("字符集不是GBK");
@@ -164,22 +164,21 @@ public class DesController {
         }
 
         /**获取对称秘钥*/
-        Key desKey = RsaUtil.getKey(JH_DES_KEY_FILE);
-        String dk = new String(Base64.encodeBase64(desKey.getEncoded()));
+        String desKey = (String) RsaUtil.getKey(JH_DES_KEY_FILE,"des");
         /**des解密*/
-        String requestMessagePriKeyDes = DesedeUtil.decrypt(requestMessage.getBytes(), dk);
+        String requestMessagePriKeyDes = DesedeUtil.decrypt(requestMessage.getBytes(), desKey);
         /**获取建行公钥*/
-        PublicKey publicKey = (PublicKey) RsaUtil.getKey(JH_PUBLIC_KEY_FILE);
+        PublicKey publicKey = (PublicKey) RsaUtil.getKey(JH_PUBLIC_KEY_FILE,"pub");
         /**验证签名*/
         boolean bool = RsaUtil.verify(publicKey, signature.getBytes(), requestMessagePriKeyDes.getBytes());
         if (bool) {
             /**私钥解密请求报文*/
-            String encodedText = RsaUtil.decrypt(requestMessagePriKeyDes, PRIVATE_KEY_FILE);
+            //String encodedText = RsaUtil.decrypt(requestMessagePriKeyDes, PRIVATE_KEY_FILE);
             /**我方接口地址*/
-            String url = "";
+            String url = "http://172.16.2.21:80/bankNotice/jianshe";
             HttpClient httpClient = new HttpClient();
             PostMethod method = new PostMethod(url);
-            method.addParameter("xml", encodedText);
+            method.addParameter("xml", requestMessagePriKeyDes);
             httpClient.getParams().setSoTimeout(3000);
             int statusCode = httpClient.executeMethod(method);
             if (statusCode != HttpStatus.SC_OK) {
@@ -187,6 +186,8 @@ public class DesController {
                 LOGGER.error("请求失败,请稍后重试,状态码为:" + statusCode);
                 return "请求失败,请稍后重试,状态码为:" + statusCode;
             } else {
+                byte[] data = method.getResponseBody();
+                LOGGER.info("data为:"+Base64.encodeBase64String(data));
                 return "请求成功！";
             }
         } else {
